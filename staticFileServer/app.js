5dfec850ae85d4621eeffcdac8030f947ebeb51d
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-02-15 16:19:05 -0700

    added part 2

--- staticFileServer/app.js
+++ staticFileServer/app.js
@@ -1,9 +1,15 @@
 // Requires \\
 var express = require('express');
 var bodyParser = require('body-parser');
+var exportObject = require('./fileSystem');
 
 // Create Express App Object \\
 var app = express();
+
+// use function - custom middle ware before static \\
+// app.use(function(req, res, next){
+//     console.log('${req.method} request for ');
+// });
 
 // Application Configuration \\
 app.use(bodyParser.json());
@@ -12,7 +18,8 @@ app.use(express.static(__dirname + '/public'));
 
 // Routes \\
 app.get('/', function(req, res){
-  res.send('Hello')
+  res.header('Content-Type', 'text/html')
+  res.send(exportObject.fileContents)
 });
 
 // Creating Server and Listening for Connections \\
--- staticFileServer/fileSystem.js
+++ staticFileServer/fileSystem.js
@@ -1,1 +1,10 @@
-var fileSystem = require('fs');
+//set up file system
+var fs = require('fs');
+
+var fileContents = fs.readFileSync('data.txt', 'utf-8');
+
+console.log(fileContents);
+
+module.exports = {
+    fileContents: fileContents
+}
